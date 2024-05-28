3d669d6c11c70b39a187562b8ccd4ea440f0c00f
JoseCob <josecobkantun123@gmail.com>
2024-05-28 15:29:31 -0600

    Actualización de diseño
    Se actualizo el diseño responsivo del login y se agrego el formulario de registro de usuarios, falta adaptar diseño al registro y su responsive correspondiente

---
+++ routes/register.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//Ruta para llamar el inicio de la página
+router.get('/', (req, res) => {
+    res.render('register', {title: 'Registro'});
+});
+
+module.exports = router
--- routes/routes.js
+++ routes/routes.js
@@ -5,9 +5,10 @@ const router = express.Router();
 //Rutas para navegar en las opciones de la app
 const index = require('./index'); //Llama al archivo index.js con la variable index
 const login = require('./login') //Llama al archivo login.js con la variable login
+const register = require('./register')
 
 router.use('/', index);//Usa el archivo index.js mediante el parametro '/' con la variable index para mostrar el resultado de la vista al usuario
 router.use('/login', login);
-
+router.use('/register', register);
 
 module.exports = router;
